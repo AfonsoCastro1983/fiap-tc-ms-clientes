30bde03f7b2bcc281dd382f5faaae7d9c8764f0e
AfonsoCastro1983 <afonso.castro@gmail.com>
2024-11-18 16:39:35 -0400

    ajuste variável banco

--- infrastructure/db/db-clientes.tf
+++ infrastructure/db/db-clientes.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "fiap-ms-cliente" {
   storage_type         = "gp2"
   engine               = "postgres"
   instance_class       = "db.t3.nano"
-  name                 = "ms-cliente"
+  db_name              = "ms-cliente"
   username             = var.db_username
   password             = var.db_password
   publicly_accessible  = true
